90f449f9f54e7064891d0e18b56d4acc089e62ae
gobindacb <p.gobinda.cb@gmail.com>
2023-01-02 00:46:34 +0600

    railway deploy

--- routes/users.js
+++ routes/users.js
@@ -16,21 +16,3 @@ router.get("/:id/friends", verifyToken, getUserFriends);
 router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
 
 export default router;
-// import express from "express";
-// import {
-//     getUser,
-//     getUserFriends,
-//     addRemoveFriend,
-// } from "../controllers/users.js";
-// import { verifyToken } from "../middleware/auth.js";
-
-// const router = express.Router();
-
-// /*Read*/
-// router.get("/:id", verifyToken, getUser);
-// router.get("/:id/friends", verifyToken, getUserFriends);
-
-// /*UPDATE*/
-// router.patch("/:id/:friendID", verifyToken, addRemoveFriend);
-
-// export default router;
